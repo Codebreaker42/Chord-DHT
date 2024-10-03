73505350ae1bb3402ff782c85fe9909c8d1f10df
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2024-10-03 22:34:31 +0530

    implementing the node class and its functions

--- node.py
+++ node.py
@@ -5,14 +5,21 @@
 class Node:
     def __init__(self, node_id, bits=160) -> None:
         """ initialize the node attributes """
-        pass
+        self.node_id = node_id
+        self.data= {}  #key value storage
+        self.finger_table= [None]*bits 
+        self.successor= None #store the successor node 
+        self.predecessor= None  #store the predecessor node
 
-    def store_data(self):
+    def store_data(self, key, value):
         """ Store the key value pair in the node"""
+        self.data[key]= value
 
-    def lookup_data(self):
+    def lookup_data(self , key:str):
         """ look up the key in the current node"""
+        return self.data.get(key,None)
+
 
     def find_successor(self):
         """Find the successor node responsible for a given key"""
-        
+        pass
