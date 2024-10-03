942b62be4c9eea5865d3a1dd627faac2b5d3dc05
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2024-10-03 22:26:41 +0530

    implement the hash function to produce the hash code for given key

--- hash_util.py
+++ hash_util.py
@@ -1,5 +1,12 @@
 """                                                  Utility Function for Hashing                                         """
 
-def hash_function(self):
+import hashlib #To change the data into the hash value
+
+def hash_function(key:str, bits=160) -> int:
     """ Hashed the key using SHA-1 and returns the integer value in the range [0,2^bits-1]"""
-    pass
+    hash_bytes= hashlib.sha1(key.encode()).digest()
+    hash_int= int.from_bytes(hash_bytes,byteorder='big')
+    return hash_int%(2**bits)
+
+# testing the code 
+# print(hash_function("node2"))
