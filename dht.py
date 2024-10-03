15d57e0a89533a070e869949c0af5bc7c2595c32
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2024-10-03 23:16:09 +0530

    distributed hash table implemented

--- dht.py
+++ dht.py
@@ -4,12 +4,18 @@ from chord import Chord
 from hash_util import hash_function
 
 class DHT:
-    def __init__(self,) -> None:
-        pass 
+    def __init__(self, chord: Chord) -> None:
+        self.chord= chord #stores the chord instance inside the DHT object.
+        
 
     def put(self,key,value):
         """ Store the key value pair in the appropriate node"""
+        key_id= hash_function(key) #determines which node in chord network responsible for storing key
+        node = self.chord.lookup(key) #find the node responsible for storing the data associated with this key
+        node.store_data(key,value)
 
     def get(self, key):
         """ Retrieve value for a key from the appropriate node"""
-        
+        key_id= hash_function(key)
+        node= self.chord.lookup(key)
+        return node.lookup_data(key)
